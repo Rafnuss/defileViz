07dcae29e4f5f9f86abf6acd4d3d2a3b56ed7076
Raphaël Nussbaumer <rafnuss@gmail.com>
2025-09-25 18:20:07 +0200

    update with https

--- src/services/netcdf.js
+++ src/services/netcdf.js
@@ -10,7 +10,7 @@ import { NetCDFReader } from "netcdfjs";
  */
 export async function fetchNetCDF(dateStr, speciesName, variableNames = ["pred_log_hourly_count"]) {
   const speciesNameUnderscored = speciesName.replace(/ /g, "_");
-  const url = `http://defile.raphaelnussbaumer.com/forecasts/${speciesNameUnderscored}/${dateStr.replace(
+  const url = `https://defile.raphaelnussbaumer.com/forecasts/${speciesNameUnderscored}/${dateStr.replace(
     /-/g,
     ""
   )}_${speciesNameUnderscored}.nc`;
